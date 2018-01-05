2d9a0d6e7e74fcdd7c2615d830ebf6863916acf6
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-01-05 12:10:09 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -18,6 +18,8 @@ def retrieval
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
+  shipping_manifest["whale bone corsets"]
+
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
